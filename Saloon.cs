11cb88adce65a8b34f4aa032f678c16db9bd41a7
Eli Waksbaum <eli@waksbaum.com>
2021-11-15 23:07:53 -0500

    saloon outline

---
+++ Saloon.cs
@@ -0,0 +1,14 @@
+using Algiers;
+
+public class Saloon : Room
+{
+    public Saloon(Player player) : base("saloon")
+    {
+        //AddExit("back room", "lair");
+
+        //AddObject(new Dealer(player));
+        //AddObject(new Bartender(player));
+
+        //JukeBox
+    }
+}
