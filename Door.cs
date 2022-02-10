4eba10246560bf888132388e379ba91413bee8ca
Eli Waksbaum <eli@waksbaum.com>
2022-02-09 22:14:06 -0500

    door cleanup

--- Door.cs
+++ Door.cs
@@ -2,45 +2,43 @@ using Algiers;
 
 public class Door : GameObject
 {
+    SPlayer player;
 
     public Door(SPlayer player) : base("door")
     {
+        this.player = player;
 
         SetTransitiveResponse("what", () => {
             return "A heavy metal door with a sign that says 'Staff Only'. It's locked with a keypad.";
         });
 
-        SetTransitiveResponse("open", Open);
-        string Open()
-        {
+        SetTransitiveResponse("open", () => {
             Parser.GetParser.GoRaw(Keypad);
             return "Enter the three-digit code for the door:";
-        }
+        });
+    }
 
-        string Keypad(string input)
+    string Keypad(string input)
+    {
+        int value = -1;
+        if ((input == "000") || (int.TryParse(input, out value) && value > 0 && value < 1000))
         {
-            int value = -1;
-            if ((input == "000") || (int.TryParse(input, out value) && value > 0 && value < 1000))
+            Parser.GetParser.GoStandard();
+            if (input == player.DoorCode)
             {
-                Parser.GetParser.GoStandard();
-                if (input == player.DoorCode)
-                {
-                    player.AddWaypoint("stage5");
-                    player.current_room.OnExit();
-                    player.current_room = new Backroom();
-                    return "You're in! blam blam";
-                }
-                else
-                {
-                    return "The door stays shut. That must not be the right code.";
-                }
+                player.AddWaypoint("stage5");
+                player.current_room.OnExit();
+                player.current_room = new Backroom();
+                return "You're in! blam blam";
             }
             else
             {
-                return "Enter three digits 0-9:";
+                return "The door stays shut. That must not be the right code.";
             }
         }
+        else
+        {
+            return "Enter three digits 0-9:";
+        }
     }
-
-    
 }
