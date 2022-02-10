e3d9b4fc7879e8e570623a9d674ec67a3a07fe31
Eli Waksbaum <eli@waksbaum.com>
2022-02-09 22:02:49 -0500

    fight alpha

--- Backroom.cs
+++ Backroom.cs
@@ -1,11 +1,85 @@
 using Algiers;
+using System;
 
 public class Backroom : Room
 {
+    State darkState = new State();
+
+    Goon kwim = new Goon("kwim", 0.0f);
+    Goon letta = new Goon("letta", 0.5f);
+    Goon skinner = new Goon("skinner", 0.0f);
+
     public Backroom() : base("backroom")
     {
+        description = "The room behind the door is pretty sparse. A single LIGHTBULB hangs from the ceiling, making it feel like a basement.";
+
         GameObject can = new GameObject("can");
         can.SetTransitiveResponse("shoot", () => {return "clang";});
         AddObject(can);
+
+        kwim.SetTransitiveResponse("what", () => {
+            return "A skinny Brotherhood goon. You think they might have shot you before, or maybe that was Letta.";
+        });
+        AddObject(kwim);
+        
+        
+        letta.SetTransitiveResponse("what", () => {
+            return "A fat Brotherhood goon. You think they might have shot you before, or maybe that was Kwim.";
+        });
+        AddObject(letta);
+        
+        skinner.SetTransitiveResponse("what", () => {
+            return "The boss in charge here. This one you've heard of. Supposedly he was there when they took Pitr.";
+        });
+        AddObject(skinner);
+
+    }
+
+    string kLook()
+    {
+        return "KWIM is standing in the left corner behind a FILING CABINET, just below a big VENT.";
+    }
+    string kTalk()
+    {
+        if (!(skinner.IsDead && letta.IsDead))
+        {
+            return "KWIM gives you a gesture that makes you think they're not interested in talking.";
+        }
+        else
+        {
+            return kwim.Talk();
+        }
+    }
+
+    string lLook()
+    {
+        return "LETTA is crouching behind the FRIDGE on the right side of the room.";
+    }
+    string lTalk()
+    {
+        if (!(skinner.IsDead && kwim.IsDead))
+        {
+            return "LETTA gives you a gesture that makes you certain they're not interested in talking.";
+        }
+        else
+        {
+            return letta.Talk();
+        }
+    }
+
+    string sLook()
+    {
+        return "SKINNER is just visible behind the COUCH.";
+    }
+    string sTalk()
+    {
+        if (!(kwim.IsDead && letta.IsDead))
+        {
+            return "'You're not talking your way out of this dickhead. I'm taking that ring back from your cold dead hands.'";
+        }
+        else
+        {
+            return skinner.Talk();
+        }
     }
 }
---
+++ Goon.cs
@@ -0,0 +1,59 @@
+using Algiers;
+using System;
+
+public class Goon : GameObject
+{
+    Random rand = new Random();
+    string name;
+    float odds;
+
+    bool isTalking;
+    int talkCount;
+    bool isDead = false;
+    public bool IsDead => isDead;
+
+    public Goon(string name, float odds) : base(name)
+    {
+        this.name = Parser.CapitalizeFirst(name);
+        this.odds = odds;
+        SetTransitiveResponse("shoot", Shoot);
+    }
+
+    string Shoot()
+    {
+        if (isTalking)
+        {
+            return "You see a moment of panic on " + name + "'s face but it doesn't last long. They join their coworkers on the floor.";
+        }
+        else
+        {
+            double roll = rand.NextDouble();
+            if (roll > odds)
+            {
+                isDead = true;
+                return "You take careful aim and " + name + " hits the floor.";
+            }
+            else
+            {
+                return "Crap. " + name + " got out of the way.";
+            }
+        }
+    }
+
+    public string Talk()
+    {
+        isTalking = true;
+        talkCount++;
+        switch (talkCount)
+        {
+            case 1:
+                return "Okay, okay. What do you want to know?";
+            case 2:
+                return "Pitr? How should I know? That was years ago!";
+            case 3:
+                return "I swear! I swear! I'm a nobody ok, they've got me out here working this small-time outpost in the middle of nowhere. I don't know where they took him!";
+            default:
+                return "I don't know! I swear!";
+        }
+    }
+}
