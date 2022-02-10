3b276fc4468bdb0b58c247581187f087073694bd
Eli Waksbaum <eli@waksbaum.com>
2022-02-09 22:07:20 -0500

    bartender cleanup

--- Bartender.cs
+++ Bartender.cs
@@ -2,73 +2,76 @@ using Algiers;
 
 public class Bartender : GameObject
 {
+    Player player;
+    bool twoTalk1;
+    bool proven;
+    
     public Bartender(Player player) : base("bartender", "the bartender")
     {
-        SetCondition("2talk1", false);
-        SetCondition("proven", false);
+        this.player = player;
 
-        string Talk()
-        {
-            if (player.HasWaypoint("stage4"))
-            {
-                return player.HasWaypoint("hascode") ? "'Can I interest you in anything else, sir?'" :
-                    "'You're looking lucky sir, you might want to play a hand. Anything to quench your thirst?'";
-            }
-            else if (player.HasWaypoint("stage3"))
-            {
-               return "You don't want to start a conversation with anyone while Rys is here."; 
-            }
-            else if (player.HasWaypoint("stage2"))
-            {
-                if (GetCondition("proven"))
-                {
-                    return "'What's your poison, sir?'";
-                }
-                else
-                {
-                    if (GetCondition("2talk1"))
-                    {
-                        return "'Look, do you want a drink or not?'";
-                    }
-                    else
-                    {
-                        SetCondition("2talk1", true);
-                        return "'Still thirsty?'";
-                    }
-                }
-            }
-            else //stage1
-            {
-                return "'I'm not your shrink, bud. Do you want a drink or not?'";
-            }
-        }
         SetTransitiveResponse("talk", Talk);
+        SetDitransitiveResponse("show", Show);
+        SetTransitiveResponse("what", () => {return "The bartender is a big, bald, mean looking guy. He stands hunched over the bar, looking bored.";});
+    }
 
-        string Show(string item)
+    string Talk()
+    {
+        if (player.HasWaypoint("stage4"))
         {
-            if (item == "signet")
+            return player.HasWaypoint("hascode") ? "'Can I interest you in anything else, sir?'" :
+                "'You're looking lucky sir, you might want to play a hand. Anything to quench your thirst?'";
+        }
+        else if (player.HasWaypoint("stage3"))
+        {
+            return "You don't want to start a conversation with anyone while Rys is here."; 
+        }
+        else if (player.HasWaypoint("stage2"))
+        {
+            if (proven)
             {
-                string phrase = "'I'm most pleased to welcome a brother in arms.'";
-                if (!GetCondition("proven"))
-                {
-                    SetCondition("proven", true);
-                    phrase += "\nThe bartender slides you a playing card.";
-
-                    GameObject card = new GameObject("two of hearts");
-                    card.SetTransitiveResponse("what", () => {
-                        return "The bartender gave you this card when you showed him the signet. What does it mean?";
-                    });
-                    player.AddToInventory(card);
-                }
-                return phrase;
+                return "'What's your poison, sir?'";
             }
             else
             {
-                return "The bartender doesn't seem interested in the " + item + ".";
+                if (twoTalk1)
+                {
+                    return "'Look, do you want a drink or not?'";
+                }
+                else
+                {
+                    twoTalk1 = true;
+                    return "'Still thirsty?'";
+                }
             }
         }
-        SetDitransitiveResponse("show", Show);
+        else //stage1
+        {
+            return "'I'm not your shrink, bud. Do you want a drink or not?'";
+        }
+    }
 
-        SetTransitiveResponse("what", () => {return "The bartender is a big, bald, mean looking guy. He stands hunched over the bar, looking bored.";});
+    string Show(string item)
+    {
+        if (item == "signet")
+        {
+            string phrase = "'I'm most pleased to welcome a brother in arms.'";
+            if (!proven)
+            {
+                proven = true;
+                phrase += "\nThe bartender slides you a playing card.";
+
+                GameObject card = new GameObject("two of hearts");
+                card.SetTransitiveResponse("what", () => {
+                    return "The bartender gave you this card when you showed him the signet. What does it mean?";
+                });
+                player.AddToInventory(card);
+            }
+            return phrase;
+        }
+        else
+        {
+            return "The bartender doesn't seem interested in the " + item + ".";
+        }
     }
 }
